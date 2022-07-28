7233692182f075a8322555db5593c90f62eeb7b2
repentandliveholy <repentandliveholy@gmail.com>
2022-07-28 15:46:39 +0300

    123

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-// the hello world program
-console.log('Hello World');
+const express = require('express')
+const app = express()
+const port = proces.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
